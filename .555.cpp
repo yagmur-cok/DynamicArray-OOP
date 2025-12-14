e99d92690cb29301f4de72bd79b8fc66db56f66a
yagmur-cok <yagmur_61_21@icloud.com>
2025-12-14 16:44:13 +0300

    Initialize project structure

---
+++ .555.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+class DynamicArray {
+public:
+    DynamicArray();
+};
+
+int main() {
+    cout << "Project initialized" << endl;
+    return 0;
+}
+
+DynamicArray::DynamicArray() {}
